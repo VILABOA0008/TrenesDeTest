16b188b239bd3054835570f6e2b3b7987c03c82d
pabcos <pablo.vilaboa@ctag.com>
2020-01-27 18:15:05 +0100

    Added optional,AsserThat and Clases Showed instead of new Files

--- src/testid/Escribir2.java
+++ src/testid/Escribir2.java
@@ -8,6 +8,7 @@ public class Escribir2 {
 
   public static void escribir(String ruta, String texto,String clase) {
 
+    System.err.println(clase+"\n\n\n"+texto+"\n\n\n\n\n");
 //    ruta="C:\\Users\\pabcos\\Documents\\FactorioTes\\src\\testsss";
     System.err.println(ruta);
     File archivo = new File(ruta);
@@ -16,9 +17,9 @@ public class Escribir2 {
     //       if(!archivo.exists()){
     try {
       FileWriter aWriter = new FileWriter(archivo, false);
-      aWriter.write(texto);
-      aWriter.flush();
-      aWriter.close();
+//      aWriter.write(texto);
+//      aWriter.flush();
+//      aWriter.close();
       System.out.println("bien");
     } catch (IOException e) {
       System.out.println("Ruta Erronea Cambiela Plis.");
--- src/testid/IdClass.java
+++ src/testid/IdClass.java
@@ -6,13 +6,13 @@ public class IdClass {
 
     String test = "package com.ctag.paperless.quality.domain.model." + clase.toLowerCase() + ";\n"
         + "\r\n" +
-        "import org.assertj.core.api.Assertions;\r\n" +
+        "import org.assertj.core.api.Assertions;\r\n"
+        + "import static org.assertj.core.api.Assertions.assertThat; " +
         "import org.junit.Before;\r\n" +
         "import org.junit.Test;\r\n" +
         "import org.junit.runner.RunWith;\r\n" +
         "import org.mockito.Mock;\r\n" +
         "import org.mockito.junit.MockitoJUnitRunner;\r\n" +
-        "import org.modelmapper.spi.MappingContext;\r\n" +
         "\r\n" +
         "import com.ctag.paperless.quality.domain.model." + clase + "." + id + ";\r\n" +
         "\r\n" +
@@ -23,9 +23,6 @@ public class IdClass {
         "public class " + id + "UnitTest {\r\n" +
         "\r\n" +
         "  private static final Integer TEST_INTEGER = 1;\r\n" +
-        "\r\n" +
-        "  @Mock\r\n" +
-        "  private MappingContext<Integer, " + id + "> mockedMappingContext;\r\n" +
         "\r\n" +
         "  private " + id + " underTest;\r\n" +
         "\r\n" +
@@ -52,9 +49,22 @@ public class IdClass {
         "  public void testId() {\r\n" +
         "    Assertions.assertThat(underTest.getId()).isNotNull().isEqualTo(TEST_INTEGER);\r\n" +
         "  }\r\n" +
+        "  \n@Test\r\n" +
+        "  public void testOptional() throws Exception {\r\n" +
+        "\r\n" +
+        "    assertThat(" + id + ".getOptional(TEST_INTEGER))\r\n" +
+        "        .isNotNull()\r\n" +
+        "        .isEqualTo(Optional.of(new " + id + "(TEST_INTEGER)));\r\n" +
+        "\r\n" +
+        "    assertThat(" + id + ".getOptional(null)).isNotNull().isEqualTo(Optional.empty());\r\n"
+        +
+        "  }" +
         "}";
 
-    Escribir2.escribir("C:\\GIT\\PSA\\Totem\\backend\\totem-specs\\src\\test\\java\\ut\\com\\ctag\\paperless\\psa\\totem\\domain\\model" + clase, test,
+    Escribir2.escribir(
+        "C:\\GIT\\PSA\\Totem\\backend\\totem-specs\\src\\test\\java\\ut\\com\\ctag\\paperless\\psa\\totem\\domain\\model"
+            + clase,
+        test,
         id + "UnitTest");
 
   }
